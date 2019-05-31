794c051f268cd431f2d93b458de6ce517ae71773
Jacob Farkas <jfarkas@lrei.org>
2019-05-31 13:57:58 -0400

    Update login-hook.sh

--- exam-lock-v2/login-hook.sh
+++ exam-lock-v2/login-hook.sh
@@ -2,4 +2,4 @@
 defaults write com.apple.loginwindow LoginHook /usr/local/exam.sh
 
 # Copies the Red Orange png file to right place
-cp Red\ Orange.png /Library/Desktop\ Pictures/Solid Colors/
+cp Red\ Orange.png /Library/Desktop\ Pictures/Solid\ Colors/
