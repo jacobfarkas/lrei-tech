1b522946af1a030ef46598a5dbd8098619c26a3e
Jacob Farkas <jfarkas@lrei.org>
2019-06-05 12:36:06 -0400

    Update office2016.sh

--- word2016/office2016.sh
+++ word2016/office2016.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Download Office 2016 using curl
-curl -o /usr/local/etc/office2016.pkg https://redacted(see slack channel for full URL)
+curl -o /usr/local/etc/office2016.pkg https://redacted(see slack channel --#sysadmin for full URL)
 
 # Install Office 2016
 installer -pkg /usr/local/office2016.pkg -target /
